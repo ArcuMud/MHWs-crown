e055c66b534f95f9553348ee1c4a59ab758eb7de
augustine <ashramwen@gmail.com>
2025-03-18 15:41:31 +0800

    feat: Add Mobs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { TooltipProvider } from '@radix-ui/react-tooltip';
 import { Mobs } from './Mobs/Mobs';
 
 function App() {
-  return <Mobs />;
+  return (
+    <TooltipProvider>
+      <Mobs />
+    </TooltipProvider>
+  );
 }
 
 export default App;
--- src/Mobs/Mobs.tsx
+++ src/Mobs/Mobs.tsx
@@ -1,12 +1,25 @@
+import { clsx } from 'clsx';
+import { MOBS } from './constants';
 import './Mobs.css';
-
-const mobList = ['chatacabra', 'quematrice', 'lala-barina', 'congalala'];
+import { Tooltip, TooltipContent, TooltipTrigger } from '@radix-ui/react-tooltip';
+import { Fragment } from 'react';
 
 export function Mobs() {
   return (
-    <>
-      <div className="mob chatacabra" />
-      <div className="mob quematrice" />
-    </>
+    <div className="mobs-grid">
+      {MOBS.map((mob) => (
+        <Fragment key={mob.className}>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <div className={clsx(['mob', mob.className])} />
+            </TooltipTrigger>
+            <TooltipContent side="top" align="center">
+              <p>{mob.zhName}</p>
+            </TooltipContent>
+          </Tooltip>
+        </Fragment>
+      ))}
+      <div className="source" />
+    </div>
   );
 }
